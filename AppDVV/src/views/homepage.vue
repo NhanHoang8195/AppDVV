b04fbfcf001cc948b55b726406c1bbf0f08cf2e3
NhanHoang <trongnhan8195@gmail.com>
2017-12-25 17:51:39 +0700

    gg map

--- AppDVV/src/views/homepage.vue
+++ AppDVV/src/views/homepage.vue
@@ -1,11 +1,8 @@
 <template>
 <div>
   <gmap-map :center="center" :zoom="7" style="width: 500px; height: 300px">
-    <gmap-marker v-for="(marker, index) in markers" :position="marker.location" :clickable="true" :draggable="true" @click="center=marker.location" id></gmap-marker>
+    <gmap-marker v-for="marker in markers" :position="marker.geometry.location" :clickable="true" :draggable="true" @click="center=marker.geometry.location"></gmap-marker>
   </gmap-map>
-  <input type="text" v-model="searchValue" />
-  <button v-on:click="search">Search</button>
-  <button v-on:click="newPos">NewPost</button>
 </div>
 </template>
 
@@ -18,37 +15,41 @@ export default {
         lat: 14.058324,
         lng: 108.277199
       },
-      markers: []
+      markers: [],
+      appdtvResponse: []
     }
   },
   methods: {
-    search: function() {
-      this.$http.get('https://maps.googleapis.com/maps/api/geocode/json', {
-        params: {
-          address: this.searchValue
-        }
-      }).then(
-        response => {
-          console.log(response);
-          //   console.log(response.body.results[0].geometry);
-          //   console.log(response.body.results[0].geometry.location.lat);
-          this.markers = response.body.results[0];
-          //   this.center.lat = response.body.results[0].geometry.location.lat;
-          //   this.center.lng = response.body.results[0].geometry.location.lng;\
 
-          // }, response => {
-          console.log(this.markers);
-          console.log(response.body.results[0].geometry);
-        }, response => (console.log(response))
-      )
-    },
-    newPos: function() {
-      console.log(this.markers);
-    }
   },
   created() {
-    // this.$http.get('http://localhost:8080/appdtvapi')
+    this.$http.get('http://localhost:8080/appdtvapi').then(response => {
+      this.appdtvResponse = response.body;
+      var index;
+      for (index = 0; index < this.appdtvResponse.length; index++) {
+        this.$http.get('https://maps.googleapis.com/maps/api/geocode/json', {
+          params: {
+            address: this.appdtvResponse[index].address
+          }
+        }).then(
+          responseGoogle => {
 
+            //   console.log(response.body.results[0].geometry);
+            //   console.log(response.body.results[0].geometry.location.lat);
+            //this.markers = responseGoogle.body.results;
+
+            this.markers.push(responseGoogle.body.results);
+            console.log(this.markers);
+            // this.center.lat = responseGoogle.body.results.geometry.location.lat;
+            // this.center.lng = responseGoogle.body.results[0].geometry.location.lng;
+
+
+          }, responseGoogle => (console.log(responseGoogle))
+        )
+      }
+    }, response => {
+      console.log(response);
+    })
 
   }
 }
