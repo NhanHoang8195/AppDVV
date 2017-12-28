3f00ef3cb6665df48ad671c8447fbb3b9d980e1a
NhanHoang <trongnhan8195@gmail.com>
2017-12-28 08:32:10 +0700

    add geocoding

--- AppDVV/src/views/homepage.vue
+++ AppDVV/src/views/homepage.vue
@@ -1,11 +1,40 @@
 <template>
-<div>
+  <div>
+    <div class="col-sm-5">
+    <gmap-map :center="center" :zoom="4" style="height: 300px">
+      <gmap-marker :key="index" v-for="(marker, index) in markers" :position="marker.location" :clickable="true" :draggable="true" @click="clickMarker(marker)" @dragend="dragMarker" :label="index.toString()"></gmap-marker>
+    </gmap-map>
+    <div>
+      <div class="input-group" style="margin-top:5px">
+        <input type="text" class="form-control" placeholder="Geocoding" onfocus="this.placeholder=''" onblur="this.placeholder='Geocoding'">
+        <span class="input-group-btn">
+          <button class="btn btn-default" type="button" style="border-left:0px"><span class="glyphicon glyphicon-search"></span></button>
+        </span>
+      </div>
+    </div>
+    <template v-if="displayInfo">
+      <div id="canvas">
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th>Số điện thoại</th>
+            <th>Địa điểm đón</th>
+          </tr>
+        </thead>
+        <tbody >
+            <td>{{obj.phone}}</td>
+            <td>{{obj.address}}</td>
+        </tbody>
+      </table>
+      </div>
+    </template>
+    </div>
+      <div class="col-sm-6">
+        fsadfasdfsad
+      </div>
 
-  <gmap-map :center="center" :zoom="4" style="width: 500px; height: 300px">
-    <gmap-marker :key="index" v-for="(marker, index) in markers" :position="marker.location" :clickable="true" :draggable="true" @click="center=marker.location"></gmap-marker>
-  </gmap-map>
-</div>
 
+  </div>
 </template>
 
 <script>
@@ -31,13 +60,24 @@ export default {
       center: {
         lat: 14.058324,
         lng: 108.277199
-      }
+      },
+      displayInfo: false,
+      obj: {}
     }
   },
   methods: {
+    dragMarker: function(event) {
+      console.log(event.latLng.lat());
+      console.log(event.latLng.lng());
+    },
+    clickMarker: function(obj) {
+      this.displayInfo=true;
+      this.obj = obj;
+    }
 
   },
   created() {
-   }
+  //  this.label=
+}
 }
 </script>
