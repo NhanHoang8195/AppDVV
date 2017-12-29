16d0f571fe41fa80245722334f7ae35710f2dd9e
NhanHoang <trongnhan8195@gmail.com>
2017-12-29 17:47:34 +0700

    Add time

--- AppDVV/src/views/homepage.vue
+++ AppDVV/src/views/homepage.vue
@@ -2,34 +2,56 @@
 <div>
   <div class="col-sm-5">
     <gmap-map :center="center" :zoom="4" style="height: 300px">
-      <gmap-marker :key="index" v-for="(marker, index) in markers" :position="marker.location" :clickable="true" :draggable="true" @click="clickMarker(marker)" @dragend="dragMarker" :label="index.toString()"></gmap-marker>
+      <gmap-marker :key="index" v-for="(marker, index) in markers" :position="marker.location" :clickable="true" :draggable="true" @click="clickMarker($event,marker)" @dragend="dragMarker($event, marker)" :label="index.toString()"></gmap-marker>
     </gmap-map>
-    <!-- <div>
-      <div class="input-group" style="margin-top:5px">
-        <input type="text" class="form-control" placeholder="Geocoding" onfocus="this.placeholder=''" onblur="this.placeholder='Geocoding'">
-        <span class="input-group-btn">
-          <button class="btn btn-default" type="button" style="border-left:0px"><span class="glyphicon glyphicon-search"></span></button>
-        </span>
-      </div>
-    </div> -->
     <template v-if="displayInfo">
-      <div id="canvas">
-      <table class="table table-hover">
+      <div>
+      <table class="table table-hover table-bordered">
         <thead>
           <tr>
-            <th>Số điện thoại</th>
-            <th>Địa điểm đón</th>
+            <th>#</th>
+            <th>Thông tin</th>
           </tr>
         </thead>
         <tbody >
+          <tr>
+            <th>Số điện thoại</th>
             <td>{{obj.phone}}</td>
+          </tr>
+          <tr>
+            <th>Địa điểm đón</th>
             <td>{{obj.address}}</td>
+          </tr>
+          <tr>
+            <td :rowspan="reverseAddress.length +1">Kết quả Reverse Geocoding</td>
+              <tr v-for="reverse in reverseAddress" >
+                <td>{{reverse.formatted_address}} <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#myModal2" @click.prevent="sendMarker(reverse)">Chọn</button></td>
+                <div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel">Bạn muốn chọn {{obj.address}} làm địa chỉ mới?</h4>
+                      </div>
+                      <div class="modal-body">
+                      fdsfas
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                        <button type="button" class="btn btn-primary" @click="saveChange(obj)" data-dismiss="modal">Save Changes</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </tr>
+
+          </tr>
         </tbody>
       </table>
       </div>
     </template>
   </div>
-  <div class="col-sm-6">
+  <div class="col-sm-7">
     <center>Danh sách khách hàng</center>
     <table class="table table-hover table-bordered">
       <thead>
@@ -37,6 +59,7 @@
           <th>Số điện thoại</th>
           <th>Địa điểm đón</th>
           <th>Status</th>
+          <th>Ghi chú</th>
           <th>Định vị lại</th>
           <th>Điều xe</th>
         </tr>
@@ -46,8 +69,11 @@
           <td>{{marker.phone}}</td>
           <td>{{marker.address}}</td>
           <td>{{marker.status}}</td>
-          <td><center><button @click.prevent="sadfasdfasdf(marker)"  class="btn btn-primary btn-sm"  data-toggle="modal" data-target="#myModal" ><span class="glyphicon glyphicon-cog glyphicon"></span></button></center></td>
-          <td><button class="btn btn-primary" >Điều xe</button></td>
+          <td>{{marker.note}}</td>
+          <td>
+            <center><button @click.prevent="sendMarker(marker)" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-cog glyphicon"></span></button></center>
+          </td>
+          <td><button class="btn btn-primary">Điều xe</button></td>
           <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
             <div class="modal-dialog" role="document">
               <div class="modal-content">
@@ -63,9 +89,9 @@
                     </span>
                   </div>
                   <div>
-                    <label>Ý bạn là: {{index}}</label>
+                    <label>Ý bạn là: </label>
                     <p v-if="newAddress">{{newAddress.formatted_address}}</p>
-                    <p v-else style="color:red">Không tìm thấy kết quả</p>
+                    <p v-if="disabled" style="color:red">Không tìm thấy kết quả</p>
                   </div>
                 </div>
                 <div class="modal-footer">
@@ -113,22 +139,40 @@ export default {
         lng: 108.277199
       },
       displayInfo: false,
+      displayDialog: false,
       obj: {},
       newAddress: {},
-      disabled:true,
-      agreechange: false // click button save change;
+      disabled: true,
+      reverseAddress: []
+
+      //  agreechange: false // click button save change;
     }
   },
   methods: {
-    dragMarker: function(event) { // keo tha vi tri hien tai
-      console.log(event.latLng.lat());
-      console.log(event.latLng.lng());
-    },
-    clickMarker: function(obj) { // click vao 1 marker hien len info cua maker do
+    dragMarker: function(event, obj) { // keo tha vi tri hien tai
+      // console.log(event.latLng.lat());
+      // console.log(event.latLng.lng());
       this.displayInfo = true;
       this.obj = obj;
+
+      this.$http.get('https://maps.googleapis.com/maps/api/geocode/json', {
+        params: {
+          latlng: event.latLng.lat().toString() + ',' + event.latLng.lng().toString()
+        }
+      }).then(responseGoogle => {
+        this.reverseAddress = responseGoogle.body.results;
+
+      }, {}).catch(() => {
+        alert('error');
+      });
+      //this.displayDialog = true;
     },
-    search: function() {  // click search button => find address
+    clickMarker: function(event, obj) { // click vao 1 marker hien len info cua maker do
+      this.displayInfo = true;
+      this.obj = obj;
+      this.dragMarker(event, obj);
+    },
+    search: function() { // click search button => find address
       this.$http.get('https://maps.googleapis.com/maps/api/geocode/json', {
         params: {
           address: this.searchValue
@@ -138,34 +182,55 @@ export default {
 
       }, {}).then(success => {
         this.newAddress = success.body.results[0];
-        if(this.newAddress.geometry ==='undefined') {
+        if (typeof this.newAddress.geometry === 'undefined') {
           this.disabled = true;
         } else {
           this.disabled = false;
         }
       }, exception => {
-        this.disabled=true;
-      }).catch( ()=>{
+        this.disabled = true;
+      }).catch(() => {
         this.disabled = true;
       });
 
     },
     saveChange: function(obj) {
       var path = 'user-info' + '/' + obj['.key'];
-      console.log(obj);
-       var newModel = db.ref(path);
-      let newDataModel= {
-        address: this.newAddress.formatted_address,
-        phone: obj.phone,
-        typeCar: obj.typeCar,
-        note: obj.note,
-        location:this.newAddress.geometry.location,
-        status: "DA-DINH-VI"
-      };
-      newModel.set(newDataModel);
+      var newModel = db.ref(path);
+      console.log(path);
+      if (this.newAddress.hasOwnProperty('formatted_address')) {
+        let newDataModel = {
+          address: this.newAddress.formatted_address,
+          phone: obj.phone,
+          typeCar: obj.typeCar,
+          date: obj.date,
+          note: obj.note,
+          location: this.newAddress.geometry.location,
+          status: "DA-DINH-VI"
+        };
+        newModel.set(newDataModel);
+        this.newAddress = {};
+        console.log('newDataModel');
+      } else {
+        delete obj['.key'];
+        newModel.set(obj);
+        console.log('Object');
+
+      }
+
+
+
     },
-    sadfasdfasdf: function(obj){
-      this.obj = obj;
+    sendMarker: function(obj) {
+      if (typeof obj.formatted_address === 'undefined') { // drag send object
+        this.obj = obj;
+        console.log('xxx');
+      } else {
+        console.log('yyy');
+        console.log(this.obj);
+        this.obj.address = obj.formatted_address;
+        this.obj.location = obj.geometry.location;
+      }
     }
 
   },
